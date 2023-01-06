00f21809d69a81bf91457d783a03232de212c228
Nadya <nadya0707@yandex.ru>
2023-01-07 00:26:15 +0300

    commit 1

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число");
+int b = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (a>b) max = a;
+if (a<b) max = b;
+Console.Write ("max=");
+Console.Write (max);
